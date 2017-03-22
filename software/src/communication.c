0a6ebe25c347058c47fbd7f1f4d69bb9226b505e
Olaf Lüke <olaf@tinkerforge.com>
2017-03-22 17:18:34 +0100

    Implement grey scale and temperature callbacks

--- software/src/communication.c
+++ software/src/communication.c
@@ -37,12 +37,18 @@ BootloaderHandleMessageResponse handle_message(const void *message, void *respon
 
 
 BootloaderHandleMessageResponse set_callback_config(const SetCallbackConfig *data) {
+	if(data->callback_config > THERMAL_IMAGING_CALLBACK_CONFIG_CALLBACK_TEMPERATURE_IMAGE) {
+		return HANDLE_MESSAGE_RESPONSE_INVALID_PARAMETER;
+	}
+
+	lepton.current_callback_config = data->callback_config;
 
 	return HANDLE_MESSAGE_RESPONSE_EMPTY;
 }
 
 BootloaderHandleMessageResponse get_callback_config(const GetCallbackConfig *data, GetCallbackConfigResponse *response) {
-	response->header.length = sizeof(GetCallbackConfigResponse);
+	response->header.length   = sizeof(GetCallbackConfigResponse);
+	response->callback_config = lepton.current_callback_config;
 
 	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
 }
@@ -53,8 +59,14 @@ BootloaderHandleMessageResponse get_callback_config(const GetCallbackConfig *dat
 bool handle_grey_scale_image_low_level_callback(void) {
 	static bool is_buffered = false;
 	static GreyScaleImageLowLevelCallback cb;
+	static uint32_t packet_payload_index = 0;
+	static LeptonPacket *lepton_packet = lepton.frame.data.packets;
 
 	if(!is_buffered) {
+		if(lepton.state != LEPTON_STATE_WRITE_FRAME) {
+			return false;
+		}
+
 		uint8_t length = 62;
 		if(lepton.image_buffer_stream_index + 62 > LEPTON_IMAGE_BUFFER_SIZE) {
 			length = LEPTON_IMAGE_BUFFER_SIZE - lepton.image_buffer_stream_index;
@@ -64,20 +76,35 @@ bool handle_grey_scale_image_low_level_callback(void) {
 			return false;
 		}
 
-//		if(lepton.image_buffer_stream_index + length <= lepton.image_buffer_receive_index) {
-		if(LEPTON_IMAGE_BUFFER_SIZE == lepton.image_buffer_receive_index) {
-			tfp_make_default_header(&cb.header, bootloader_get_uid(), sizeof(GreyScaleImageLowLevelCallback), FID_CALLBACK_GREY_SCALE_IMAGE_LOW_LEVEL);
+		tfp_make_default_header(&cb.header, bootloader_get_uid(), sizeof(GreyScaleImageLowLevelCallback), FID_CALLBACK_GREY_SCALE_IMAGE_LOW_LEVEL);
+		cb.stream_chunk_offset = lepton.image_buffer_stream_index;
 
-			cb.stream_chunk_offset = lepton.image_buffer_stream_index;
-			memcpy(cb.stream_chunk_data, lepton.image_buffer + lepton.image_buffer_stream_index, length);
-			lepton.image_buffer_stream_index += length;
-#if 0
-			if(lepton.image_buffer_stream_index >= LEPTON_IMAGE_BUFFER_SIZE) {
-				lepton.image_buffer_stream_index = 0;
+		if(packet_payload_index + length > LEPTON_PACKET_PAYLOAD_SIZE) {
+			uint32_t length_first_packet = LEPTON_PACKET_PAYLOAD_SIZE - packet_payload_index;
+			for(uint8_t i = 0; i < length_first_packet; i++) {
+				cb.stream_chunk_data[i] = lepton_packet->vospi.payload[packet_payload_index];
+				packet_payload_index++;
 			}
-#endif
+
+			lepton_packet++;
+			packet_payload_index = 0;
+			for(uint8_t i = length_first_packet; i < length; i++) {
+				cb.stream_chunk_data[i] = lepton_packet->vospi.payload[packet_payload_index];
+				packet_payload_index++;
+			}
 		} else {
-			return false;
+			for(uint8_t i = 0; i < length; i++) {
+				cb.stream_chunk_data[i] = lepton_packet->vospi.payload[packet_payload_index];
+				packet_payload_index++;
+			}
+		}
+
+		lepton.image_buffer_stream_index += length;
+		if(lepton.image_buffer_stream_index == LEPTON_IMAGE_BUFFER_SIZE) {
+			lepton.state = LEPTON_STATE_READ_FRAME;
+			lepton.image_buffer_stream_index = 0;
+			lepton_packet = lepton.frame.data.packets;
+			packet_payload_index = 0;
 		}
 	}
 
@@ -88,19 +115,59 @@ bool handle_grey_scale_image_low_level_callback(void) {
 	} else {
 		is_buffered = true;
 	}
-
 	return false;
 }
 
 bool handle_temperature_image_low_level_callback(void) {
 	static bool is_buffered = false;
 	static TemperatureImageLowLevelCallback cb;
+	static uint32_t packet_payload_index = 0;
+	static LeptonPacket *lepton_packet = lepton.frame.data.packets;
 
 	if(!is_buffered) {
-		//tfp_make_default_header(&cb.header, bootloader_get_uid(), sizeof(TemperatureImageLowLevelCallback), FID_CALLBACK_TEMPERATURE_IMAGE_LOW_LEVEL);
-		// TODO: Implement TemperatureImageLowLevel callback handling
+		if(lepton.state != LEPTON_STATE_WRITE_FRAME) {
+			return false;
+		}
 
-		return false;
+		uint8_t length = 31;
+		if(lepton.image_buffer_stream_index + 31 > LEPTON_IMAGE_BUFFER_SIZE) {
+			length = LEPTON_IMAGE_BUFFER_SIZE - lepton.image_buffer_stream_index;
+		}
+
+		if(length == 0) {
+			return false;
+		}
+
+		tfp_make_default_header(&cb.header, bootloader_get_uid(), sizeof(TemperatureImageLowLevelCallback), FID_CALLBACK_TEMPERATURE_IMAGE_LOW_LEVEL);
+		cb.stream_chunk_offset = lepton.image_buffer_stream_index;
+
+		if(packet_payload_index + length > LEPTON_PACKET_PAYLOAD_SIZE) {
+			uint32_t length_first_packet = LEPTON_PACKET_PAYLOAD_SIZE - packet_payload_index;
+			for(uint8_t i = 0; i < length_first_packet; i++) {
+				cb.stream_chunk_data[i] = lepton_packet->vospi.payload[packet_payload_index];
+				packet_payload_index++;
+			}
+
+			lepton_packet++;
+			packet_payload_index = 0;
+			for(uint8_t i = length_first_packet; i < length; i++) {
+				cb.stream_chunk_data[i] = lepton_packet->vospi.payload[packet_payload_index];
+				packet_payload_index++;
+			}
+		} else {
+			for(uint8_t i = 0; i < length; i++) {
+				cb.stream_chunk_data[i] = lepton_packet->vospi.payload[packet_payload_index];
+				packet_payload_index++;
+			}
+		}
+
+		lepton.image_buffer_stream_index += length;
+		if(lepton.image_buffer_stream_index == LEPTON_IMAGE_BUFFER_SIZE) {
+			lepton.state = LEPTON_STATE_READ_FRAME;
+			lepton.image_buffer_stream_index = 0;
+			lepton_packet = lepton.frame.data.packets;
+			packet_payload_index = 0;
+		}
 	}
 
 	if(bootloader_spitfp_is_send_possible(&bootloader_status.st)) {
