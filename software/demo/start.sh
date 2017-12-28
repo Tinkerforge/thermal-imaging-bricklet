20890f26dc562505327675e74b7abe8322df2886
Lukas Lauer <lukas@doppel-l.de>
2017-12-28 10:15:01 +0100

    Add autostart scripts

---
+++ software/demo/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+bash /home/pi/Desktop/demo/startstart.sh
+
+while true ; do sleep 1; done
+
+
---
+++ software/demo/startstart.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sleep 7
+python /home/pi/Desktop/demo/main.py -fullscreen
+
+
+
