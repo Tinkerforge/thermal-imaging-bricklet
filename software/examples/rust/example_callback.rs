b13ca4dddc519ccab313fbe22681a17d515f2d97
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:31:41 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -12,9 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
                                           // Don't use device before ipcon is connected
 
-    //Create receiver for high contrast image events.
-    let high_contrast_image_receiver = ti.get_high_contrast_image_receiver();
-    // Spawn thread to handle received events. This thread ends when the `ti` object
+    
+    let high_contrast_image_receiver = ti.get_high_contrast_image_callback_receiver();
+    
+    // Spawn thread to handle received callback messages. This thread ends when the `ti` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for high_contrast_image in high_contrast_image_receiver {
