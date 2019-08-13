05d3f124d8f588999de4d53cce52dae2f5ee23d6
Olaf Lüke <olaf@tinkerforge.com>
2019-08-13 14:38:51 +0200

    spaces -> tabs

--- software/src/lepton.h
+++ software/src/lepton.h
@@ -159,186 +159,186 @@
 #define LEPTON_CID_RAD_LOW_GAIN_RBFO_EXTERNAL_PAR  (uint16_t)(LEPTON_RAD_MODULE_BASE + 0x00D8)
 
 typedef enum {
-    LEPTON_AGC_LINEAR = 0,
-    LEPTON_AGC_HEQ,
+	LEPTON_AGC_LINEAR = 0,
+	LEPTON_AGC_HEQ,
 } LeptonAGCPolicy;
 
 typedef enum {
-    LEPTON_SYSTEM_READY = 0,
-    LEPTON_SYSTEM_INITIALIZING,
-    LEPTON_SYSTEM_IN_LOW_POWER_MODE,
-    LEPTON_SYSTEM_GOING_INTO_STANDBY,
-    LEPTON_SYSTEM_FLAT_FIELD_IN_PROCESS
+	LEPTON_SYSTEM_READY = 0,
+	LEPTON_SYSTEM_INITIALIZING,
+	LEPTON_SYSTEM_IN_LOW_POWER_MODE,
+	LEPTON_SYSTEM_GOING_INTO_STANDBY,
+	LEPTON_SYSTEM_FLAT_FIELD_IN_PROCESS
 } LeptonSysCamStatusStates;
 
 typedef enum {
-    LEPTON_TELEMETRY_LOCATION_HEADER = 0,
-    LEPTON_TELEMETRY_LOCATION_FOOTER
+	LEPTON_TELEMETRY_LOCATION_HEADER = 0,
+	LEPTON_TELEMETRY_LOCATION_FOOTER
 } LeptonSysTelemetryLocation;
 
 typedef enum {
-    LEPTON_SYS_FA_DIV_1 = 0,
-    LEPTON_SYS_FA_DIV_2,
-    LEPTON_SYS_FA_DIV_4,
-    LEPTON_SYS_FA_DIV_8,
-    LEPTON_SYS_FA_DIV_16,
-    LEPTON_SYS_FA_DIV_32,
-    LEPTON_SYS_FA_DIV_64,
-    LEPTON_SYS_FA_DIV_128
+	LEPTON_SYS_FA_DIV_1 = 0,
+	LEPTON_SYS_FA_DIV_2,
+	LEPTON_SYS_FA_DIV_4,
+	LEPTON_SYS_FA_DIV_8,
+	LEPTON_SYS_FA_DIV_16,
+	LEPTON_SYS_FA_DIV_32,
+	LEPTON_SYS_FA_DIV_64,
+	LEPTON_SYS_FA_DIV_128
 } LeptonSys_Frame_Average;
 
 typedef enum {
-    LEPTON_SYS_SHUTTER_POSITION_UNKNOWN = -1,
-    LEPTON_SYS_SHUTTER_POSITION_IDLE = 0,
-    LEPTON_SYS_SHUTTER_POSITION_OPEN,
-    LEPTON_SYS_SHUTTER_POSITION_CLOSED,
-    LEPTON_SYS_SHUTTER_POSITION_BRAKE_ON
+	LEPTON_SYS_SHUTTER_POSITION_UNKNOWN = -1,
+	LEPTON_SYS_SHUTTER_POSITION_IDLE = 0,
+	LEPTON_SYS_SHUTTER_POSITION_OPEN,
+	LEPTON_SYS_SHUTTER_POSITION_CLOSED,
+	LEPTON_SYS_SHUTTER_POSITION_BRAKE_ON
 } LeptonSysShutterPosition;
 
 typedef enum {
-    LEPTON_SYS_FFC_SHUTTER_MODE_MANUAL = 0,
-    LEPTON_SYS_FFC_SHUTTER_MODE_AUTO,
-    LEPTON_SYS_FFC_SHUTTER_MODE_EXTERNAL
+	LEPTON_SYS_FFC_SHUTTER_MODE_MANUAL = 0,
+	LEPTON_SYS_FFC_SHUTTER_MODE_AUTO,
+	LEPTON_SYS_FFC_SHUTTER_MODE_EXTERNAL
 } LeptonSysFFCShutterModeState;
 
 typedef enum {
-    LEPTON_SYS_SHUTTER_LOCKOUT_INACTIVE = 0,
-    LEPTON_SYS_SHUTTER_LOCKOUT_HIGH,
-    LEPTON_SYS_SHUTTER_LOCKOUT_LOW
+	LEPTON_SYS_SHUTTER_LOCKOUT_INACTIVE = 0,
+	LEPTON_SYS_SHUTTER_LOCKOUT_HIGH,
+	LEPTON_SYS_SHUTTER_LOCKOUT_LOW
 } LeptonSysShutterTempLockoutState;
 
 typedef enum {
-    LEPTON_AGC_SCALE_TO_8_BITS = 0,
-    LEPTON_AGC_SCALE_TO_14_BITS
+	LEPTON_AGC_SCALE_TO_8_BITS = 0,
+	LEPTON_AGC_SCALE_TO_14_BITS
 } LeptonAGCHEQScaleFactor;
 
 typedef enum {
-    LEPTON_SYS_FFC_STATUS_WRITE_ERROR = -2,
-    LEPTON_SYS_FFC_STATUS_ERROR = -1,
-    LEPTON_SYS_FFC_STATUS_READY = 0,
-    LEPTON_SYS_FFC_STATUS_BUSY,
-    LEPTON_SYS_FRAME_AVERAGE_COLLECTING_FRAMES,
+	LEPTON_SYS_FFC_STATUS_WRITE_ERROR = -2,
+	LEPTON_SYS_FFC_STATUS_ERROR = -1,
+	LEPTON_SYS_FFC_STATUS_READY = 0,
+	LEPTON_SYS_FFC_STATUS_BUSY,
+	LEPTON_SYS_FRAME_AVERAGE_COLLECTING_FRAMES,
 } LeptonSysFFCStatus;
 
 
 typedef enum {
-    LEPTON_VID_WHITE_HOT = 0,
-    LEPTON_VID_BLACK_HOT
+	LEPTON_VID_WHITE_HOT = 0,
+	LEPTON_VID_BLACK_HOT
 } LeptonVIDPolarity;
 
 typedef enum {
-    LEPTON_VID_WHEEL6_LUT = 0,
-    LEPTON_VID_FUSION_LUT,
-    LEPTON_VID_RAINBOW_LUT,
-    LEPTON_VID_GLOBOW_LUT,
-    LEPTON_VID_SEPIA_LUT,
-    LEPTON_VID_COLOR_LUT,
-    LEPTON_VID_ICE_FIRE_LUT,
-    LEPTON_VID_RAIN_LUT,
-    LEPTON_VID_USER_LUT,
+	LEPTON_VID_WHEEL6_LUT = 0,
+	LEPTON_VID_FUSION_LUT,
+	LEPTON_VID_RAINBOW_LUT,
+	LEPTON_VID_GLOBOW_LUT,
+	LEPTON_VID_SEPIA_LUT,
+	LEPTON_VID_COLOR_LUT,
+	LEPTON_VID_ICE_FIRE_LUT,
+	LEPTON_VID_RAIN_LUT,
+	LEPTON_VID_USER_LUT,
 } LeptonVIDPcolorLUT;
 
 typedef enum {
-    LEPTON_OK = 0,                           // Camera ok
-    LEPTON_COMM_OK = LEPTON_OK,              // Camera comm ok (same as LEPTON_OK)
+	LEPTON_OK = 0,                           // Camera ok
+	LEPTON_COMM_OK = LEPTON_OK,              // Camera comm ok (same as LEPTON_OK)
 
-    LEPTON_ERROR = -1,                       // Camera general error
-    LEPTON_NOT_READY = -2,                   // Camera not ready error
-    LEPTON_RANGE_ERROR = -3,                 // Camera range error
-    LEPTON_CHECKSUM_ERROR = -4,              // Camera checksum error
-    LEPTON_BAD_ARG_POINTER_ERROR = -5,       // Camera Bad argument error
-    LEPTON_DATA_SIZE_ERROR = -6,             // Camera byte count error
-    LEPTON_UNDEFINED_FUNCTION_ERROR = -7,    // Camera undefined function error
-    LEPTON_FUNCTION_NOT_SUPPORTED = -8,      // Camera function not yet supported error
+	LEPTON_ERROR = -1,                       // Camera general error
+	LEPTON_NOT_READY = -2,                   // Camera not ready error
+	LEPTON_RANGE_ERROR = -3,                 // Camera range error
+	LEPTON_CHECKSUM_ERROR = -4,              // Camera checksum error
+	LEPTON_BAD_ARG_POINTER_ERROR = -5,       // Camera Bad argument error
+	LEPTON_DATA_SIZE_ERROR = -6,             // Camera byte count error
+	LEPTON_UNDEFINED_FUNCTION_ERROR = -7,    // Camera undefined function error
+	LEPTON_FUNCTION_NOT_SUPPORTED = -8,      // Camera function not yet supported error
 
-    // OTP access errors
-    LEPTON_OTP_WRITE_ERROR = -15,            // Camera OTP write error
-    LEPTON_OTP_READ_ERROR = -16,             // double bit error detected (uncorrectible)
+	// OTP access errors
+	LEPTON_OTP_WRITE_ERROR = -15,            // Camera OTP write error
+	LEPTON_OTP_READ_ERROR = -16,             // double bit error detected (uncorrectible)
 
-    LEPTON_OTP_NOT_PROGRAMMED_ERROR = -18,   // Flag read as non-zero
+	LEPTON_OTP_NOT_PROGRAMMED_ERROR = -18,   // Flag read as non-zero
 
-    // I2C Errors
-    LEPTON_ERROR_I2C_BUS_NOT_READY = -20,    // I2C Bus Error - Bus Not Avaialble
-    LEPTON_ERROR_I2C_BUFFER_OVERFLOW = -22,  // I2C Bus Error - Buffer Overflow
-    LEPTON_ERROR_I2C_ARBITRATION_LOST = -23, // I2C Bus Error - Bus Arbitration Lost
-    LEPTON_ERROR_I2C_BUS_ERROR = -24,        // I2C Bus Error - General Bus Error
-    LEPTON_ERROR_I2C_NACK_RECEIVED = -25,    // I2C Bus Error - NACK Received
-    LEPTON_ERROR_I2C_FAIL = -26,             // I2C Bus Error - General Failure
+	// I2C Errors
+	LEPTON_ERROR_I2C_BUS_NOT_READY = -20,    // I2C Bus Error - Bus Not Avaialble
+	LEPTON_ERROR_I2C_BUFFER_OVERFLOW = -22,  // I2C Bus Error - Buffer Overflow
+	LEPTON_ERROR_I2C_ARBITRATION_LOST = -23, // I2C Bus Error - Bus Arbitration Lost
+	LEPTON_ERROR_I2C_BUS_ERROR = -24,        // I2C Bus Error - General Bus Error
+	LEPTON_ERROR_I2C_NACK_RECEIVED = -25,    // I2C Bus Error - NACK Received
+	LEPTON_ERROR_I2C_FAIL = -26,             // I2C Bus Error - General Failure
 
-    // Processing Errors
-    LEPTON_DIV_ZERO_ERROR = -80,             // Attempted div by zero
+	// Processing Errors
+	LEPTON_DIV_ZERO_ERROR = -80,             // Attempted div by zero
 
-    // Comm Errors
-    LEPTON_COMM_PORT_NOT_OPEN = -101,        // Comm port not open
-    LEPTON_COMM_INVALID_PORT_ERROR = -102,   // Comm port no such port error
-    LEPTON_COMM_RANGE_ERROR = -103,          // Comm port range error
-    LEPTON_ERROR_CREATING_COMM = -104,       // Error creating comm
-    LEPTON_ERROR_STARTING_COMM = -105,       // Error starting comm
-    LEPTON_ERROR_CLOSING_COMM = -106,        // Error closing comm
-    LEPTON_COMM_CHECKSUM_ERROR = -107,       // Comm checksum error
-    LEPTON_COMM_NO_DEV = -108,               // No comm device
-    LEPTON_TIMEOUT_ERROR = -109,             // Comm timeout error
-    LEPTON_COMM_ERROR_WRITING_COMM = -110,   // Error writing comm
-    LEPTON_COMM_ERROR_READING_COMM = -111,   // Error reading comm
-    LEPTON_COMM_COUNT_ERROR = -112,          // Comm byte count error
+	// Comm Errors
+	LEPTON_COMM_PORT_NOT_OPEN = -101,        // Comm port not open
+	LEPTON_COMM_INVALID_PORT_ERROR = -102,   // Comm port no such port error
+	LEPTON_COMM_RANGE_ERROR = -103,          // Comm port range error
+	LEPTON_ERROR_CREATING_COMM = -104,       // Error creating comm
+	LEPTON_ERROR_STARTING_COMM = -105,       // Error starting comm
+	LEPTON_ERROR_CLOSING_COMM = -106,        // Error closing comm
+	LEPTON_COMM_CHECKSUM_ERROR = -107,       // Comm checksum error
+	LEPTON_COMM_NO_DEV = -108,               // No comm device
+	LEPTON_TIMEOUT_ERROR = -109,             // Comm timeout error
+	LEPTON_COMM_ERROR_WRITING_COMM = -110,   // Error writing comm
+	LEPTON_COMM_ERROR_READING_COMM = -111,   // Error reading comm
+	LEPTON_COMM_COUNT_ERROR = -112,          // Comm byte count error
 
-    // Other Errors
-    LEPTON_OPERATION_CANCELED = -126,        // Camera operation canceled
-    LEPTON_UNDEFINED_ERROR_CODE = -127       // Undefined error
+	// Other Errors
+	LEPTON_OPERATION_CANCELED = -126,        // Camera operation canceled
+	LEPTON_UNDEFINED_ERROR_CODE = -127       // Undefined error
 } LeptonResult;
 
 
 typedef struct {
-    uint16_t start_col;
-    uint16_t start_row;
-    uint16_t end_col;
-    uint16_t end_row;
+	uint16_t start_col;
+	uint16_t start_row;
+	uint16_t end_col;
+	uint16_t end_row;
 } LeptonAGCHistogramROI;
 
 typedef struct {
-    uint16_t min_intensity;
-    uint16_t max_intensity;
-    uint16_t mean_intensity;
-    uint16_t num_pixels; // def: 4800
+	uint16_t min_intensity;
+	uint16_t max_intensity;
+	uint16_t mean_intensity;
+	uint16_t num_pixels; // def: 4800
 } LeptonAGCHistogramStatistics;
 
 typedef struct {
-    uint32_t cam_status; // LeptonSysCamStatusStates
-    uint16_t command_count;
-    uint16_t reserved;
+	uint32_t cam_status; // LeptonSysCamStatusStates
+	uint16_t command_count;
+	uint16_t reserved;
 } LeptonSysCamStatus;
 
 typedef struct {
-    uint16_t mean_intensity;
-    uint16_t max_intensity;
-    uint16_t min_intensity;
-    uint16_t num_pixels;
+	uint16_t mean_intensity;
+	uint16_t max_intensity;
+	uint16_t min_intensity;
+	uint16_t num_pixels;
 } LeptonSysSceneStatistics;
 
 typedef struct {
-    uint16_t start_col;
-    uint16_t start_row;
-    uint16_t end_col;
-    uint16_t end_row;
+	uint16_t start_col;
+	uint16_t start_row;
+	uint16_t end_col;
+	uint16_t end_row;
 } LeptonSysSceneROI;
 
 typedef struct {
-    uint32_t shutter_mode;                // LEPTON_SYS_FFC_SHUTTER_MODE_STATE def:LEPTON_SYS_FFC_SHUTTER_MODE_EXTERNAL
-    uint32_t temp_lockout_state;          // LEPTON_SYS_SHUTTER_TEMP_LOCKOUT_STATE def:LEPTON_SYS_SHUTTER_LOCKOUT_INACTIVE
-    uint32_t video_freeze_during_ffc;     // bool def:enabled
-    uint32_t ffc_desired;                 // bool def:disabled
-    uint32_t elapsed_time_since_last_ffc; // (ms)
-    uint32_t desired_ffc_period;          // def:300000 (ms)
-    uint32_t explicit_cmd_to_open;        // bool def:disabled
-    uint16_t desired_ffc_temp_delta;      // def:300 (kelvins*100)
-    uint16_t imminent_delay;              // def:52 (frame counts)
+	uint32_t shutter_mode;                // LEPTON_SYS_FFC_SHUTTER_MODE_STATE def:LEPTON_SYS_FFC_SHUTTER_MODE_EXTERNAL
+	uint32_t temp_lockout_state;          // LEPTON_SYS_SHUTTER_TEMP_LOCKOUT_STATE def:LEPTON_SYS_SHUTTER_LOCKOUT_INACTIVE
+	uint32_t video_freeze_during_ffc;     // bool def:enabled
+	uint32_t ffc_desired;                 // bool def:disabled
+	uint32_t elapsed_time_since_last_ffc; // (ms)
+	uint32_t desired_ffc_period;          // def:300000 (ms)
+	uint32_t explicit_cmd_to_open;        // bool def:disabled
+	uint16_t desired_ffc_temp_delta;      // def:300 (kelvins*100)
+	uint16_t imminent_delay;              // def:52 (frame counts)
 } LeptonSysFFCShutterMode;
 
 typedef struct {
-    uint8_t reserved;
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
+	uint8_t reserved;
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
 } LeptonVIDLUTPixel;
 
 typedef struct {
@@ -346,10 +346,10 @@ typedef struct {
 } LeptonVIDLUTBuffer;
 
 typedef struct {
-    uint16_t start_col;
-    uint16_t start_row;
-    uint16_t end_col;
-    uint16_t end_row;
+	uint16_t start_col;
+	uint16_t start_row;
+	uint16_t end_col;
+	uint16_t end_row;
 } LeptonVIDFocusROI;
 
 #define LEPTON_FRAME_ROWS 60
@@ -521,7 +521,7 @@ typedef struct {
 	uint8_t resolution;
 	bool manual_transfer_ongoing;
 
-    LeptonStatistics statistics;
+	LeptonStatistics statistics;
 } Lepton;
 
 void lepton_init(Lepton *lepton);
